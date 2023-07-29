6749fb02c4ff6eeee1f9b675f2d526d22301fff0
Shizi1999 <daungocduc1999@gmail.com>
2023-07-30 00:28:12 +0700

    Vercel config

--- index.js
+++ index.js
@@ -2,10 +2,17 @@ const express = require("express");
 const cors = require("cors");
 const path = require("path");
 const connectDB = require("./src/db/ConnectDB");
-const createRoute = require("./src/routes");
+const authRoute = require("./src/routes/AuthRoute");
+const postRoute = require("./src/routes/PostRoute");
+const adminRoute = require("./src/routes/AdminRoute");
+const userRoute = require("./src/routes/UserRoute");
+const homeRoute = require("./src/routes/HomeRoute");
+const authMiddleware = require("./src/middlewares/authMiddleware");
+const adminMiddleware = require("./src/middlewares/adminMiddleware");
 const hbs = require("express-handlebars");
 const dotenv = require("dotenv");
 const envFile = `.env.${process.env.NODE_ENV || "development"}`;
+
 dotenv.config({ path: envFile });
 
 const app = express();
@@ -25,7 +32,11 @@ app.use(express.json());
 // DB
 connectDB();
 // Route
-createRoute(app);
+app.use("/api/auth", authRoute);
+app.use("/api/post", authMiddleware, postRoute);
+app.use("/api/user", authMiddleware, userRoute);
+app.use("/api/admin", authMiddleware, adminMiddleware, adminRoute);
+app.use("/api/public", homeRoute);
 app.get("/", (req, res, next) => {
   res.json("Hello, We are Connect Hope");
 });
